003c708457e7c0dbd1c3b2cd9c93fcfed8427b8e
RTLer <hooman@rtler.com>
2017-11-13 13:09:44 +0330

    fix electron configs

--- src/load_panel.sh
+++ src/load_panel.sh
@@ -1,7 +1,10 @@
+#!/usr/bin/env bash
 git clone https://github.com/ZarinPal/Panel.git html
+git pull
 cd html
 git apply ../disable_history_mode.patch
 yarn install
 yarn run production
 cp -r assets ../
 cp index.html ../
+rm -rf ../html/
